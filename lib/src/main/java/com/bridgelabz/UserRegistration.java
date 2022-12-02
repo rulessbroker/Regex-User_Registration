3d449133a66bf975f392927dd2fdb3f8b71165c5
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-12-02 11:12:00 +0530

    Added: Should all email samples valid

--- lib/src/main/java/com/bridgelabz/UserRegistration.java
+++ lib/src/main/java/com/bridgelabz/UserRegistration.java
@@ -30,17 +30,34 @@ public class UserRegistration {
 	public void checkEmail() {
 		System.out.println("Enter Email : ");
 		String email = sc.nextLine();
-		if (Pattern.matches("^[a-z]+([a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$", email)) {
+		if (Pattern.matches(
+				"^[a-z]+([a-zA-Z0-9]+)*[.]*([a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$", email)) {
 			System.out.println("Email is regestried sucessfully");
 		} else {
 			System.out.println("Email pattern is not matched");
+		}
+
+		String[] validEmail = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+				"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+		for (String s : validEmail) {
+			if (email.equals(s)) {
+				System.out.println("email is valid");
+			}
+		}
+		String[] inValidEmail = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+				"abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+				"abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+		for (String s : inValidEmail) {
+			if (email.equals(s)) {
+				System.out.println("email is invalid");
+			}
 		}
 	}
 
 	public void checkPhoneNo() {
 		System.out.println("Enter Phone No : ");
 		String phoneNo = sc.nextLine();
-		if (Pattern.matches("^(91)?\\s{0,1}[1-9]{1}[0-9]{9}$", phoneNo)) {
+		if (Pattern.matches("^([1-9]{1}[0-9])?\\s{0,1}[1-9]{1}[0-9]{9}$", phoneNo)) {
 			System.out.println("Phone Number is regestried sucessfully");
 		} else {
 			System.out.println("Phone Number pattern is not matched");
