c7096eab088ed8f1ecee47037effc28d3ed38860
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-12-07 22:47:35 +0530

    Added: JUnit parameterised test to validate multiple entry for email address

--- lib/src/test/java/lib/UserRegistrationTest.java
+++ lib/src/test/java/lib/UserRegistrationTest.java
@@ -3,6 +3,8 @@ package lib;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.bridgelabz.UserRegistration;
 
@@ -45,4 +47,18 @@ class UserRegistrationTest {
 		Assertions.assertTrue(result);
 	}
 
+	@ParameterizedTest
+	@ValueSource(strings = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+			"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" })
+	public void givenEmail_WhenValid_ShouldReturnTrue(boolean email) {
+		Assertions.assertTrue(email);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com",
+			"abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+			"abc@gmail.com.1a", "abc@gmail.com.aa.au" })
+	public void givenEmail_WhenInValid_ShouldReturnFalse(boolean email) {
+		Assertions.assertFalse(email);
+	}
 }
